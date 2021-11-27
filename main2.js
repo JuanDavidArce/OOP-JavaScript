043adb0318c106b2de9321c8bc323aea415381fe
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-27 12:09:39 -0500

    Generation of classes

--- main2.js
+++ main2.js
@@ -1,3 +1,49 @@
+class Course
+{
+    constructor({name,classes=[]})
+    {
+        this.name=name;
+        this.classes=classes;
+
+    }
+}
+
+
+const cursoProgramacionBasica = new Course({name:'curso gratis de programacion basica',});
+const cursoDefinitivoHtml = new Course({name:'curso definitivo html y css',});
+const cursoPracticoHtml = new Course({name:'curso practico html y css',});
+const cursoDataBussines = new Course({name:'curso de data bussines',});
+const cursoUnity = new Course({name:'curso de unity',});
+const cursoUnreal = new Course({name:'curso de unreal',});
+
+
+class LearningPath
+{
+    constructor({name,courses=[]})
+    {
+        this.name=name;
+        this.courses=courses;
+
+    }
+}
+
+const escuelaWeb = new LearningPath({
+    name:'Escuela de data Science',
+    courses:[cursoProgramacionBasica,cursoDefinitivoHtml,cursoPracticoHtml]
+});
+const escuelaData =   new LearningPath({
+    name:'Escuela de data Scientist',
+    courses:[cursoProgramacionBasica,cursoDataBussines]
+});
+const escuelaVgs = new LearningPath({
+    name:'curso de videojuegos',
+    courses:[cursoUnity,cursoUnreal]
+});
+
+
+
+
+
 class Student
 {
     constructor 
@@ -28,7 +74,8 @@ const juan1= new Student(
         name : 'Juan David',
         username:'juanda',
         email:'juandavid@juandavid.com',
-        twitter:'juandavidarce_'
+        twitter:'juandavidarce_',
+        learningPaths:[escuelaWeb,escuelaVgs,]
     }
 )
 
@@ -37,6 +84,7 @@ const miguelito1= new Student(
         name : 'miguelito',
         username:'miguelito',
         email:'miguelito@jmiguelito.com',
-        instagram:'miguelito_'
+        instagram:'miguelito_',
+        learningPaths:[escuelaData,escuelaWeb]
     }
 )
