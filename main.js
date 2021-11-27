e134375097e903c7e680673dd798d725823aeb86
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-26 23:03:37 -0500

    Class in javascript

--- main.js
+++ main.js
@@ -33,4 +33,29 @@ Student.prototype.aprobarCurso = function (nuevoCurso) {
     this.cursosAprobados.push(nuevoCurso);
 }
 
-const juanita = new Student('Juanita Alejandra',15,['Curso de introduccion a los videojuegos','creacion de personajes'])
+const juanita = new Student('Juanita Alejandra',15,['Curso de introduccion a los videojuegos','creacion de personajes']);
+
+
+//Prototipos con la sintaxis de clases
+
+class Student2 
+{   
+    // Es como el init en python
+    constructor ({name,age,cursosAprobados,})
+    {
+        this.name= name;
+        this.age=age;
+        this.cursosAprobados=cursosAprobados;
+    }
+
+    aprobarCurso(nuevoCurso)
+    {
+        this.cursosAprobados.push(nuevoCurso);
+    }
+};
+
+const miguelito= new Student2({
+    name:'Miguel',
+    age:28,
+    cursosAprobados:['Curso de analisis de negocios para ds',
+    'Principios de visualizacion de datos']});
